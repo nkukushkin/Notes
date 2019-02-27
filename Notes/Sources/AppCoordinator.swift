4bd55ac4e2aecb5181c5b6fb5496a78c59b27205
Nikita Kukushkin <nkukushkindev@ya.ru>
2019-02-27 14:47:58 +0300

    Switch note storage class based on config

--- Notes/Sources/AppCoordinator.swift
+++ Notes/Sources/AppCoordinator.swift
@@ -2,7 +2,14 @@ import UIKit
 
 class AppCoordinator {
     private lazy var window = UIWindow()
-    private lazy var noteStorage = UnstableNoteStorage(noteGenerator: RandomNoteGenerator())
+
+    private lazy var noteStorage: NoteStorage = {
+        #if DEBUG
+        return UnstableNoteStorage(noteGenerator: RandomNoteGenerator())
+        #else
+        return NoteStorage()
+        #endif
+    }()
 
     func launch() {
         let notesCoordinator = NotesCoordinator(noteStorage: noteStorage)
